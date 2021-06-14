a66206d7c836c8da1b08c4b0d3c8b95cfca04af3
Daniel Luz <dev.gsz@mernen.com>
2021-06-13 23:38:09 -0300

    Add more tests

--- tests/completion-rake/test.sh
+++ tests/completion-rake/test.sh
@@ -4,31 +4,31 @@ source "$(dirname "$0")/../test-setup"
 source "$(dirname "$0")/../../completion-rake"
 
 _TEST_FN=__rake
-
-set -x
-ruby --version
-rails --version
-rake --version
-set +x
+_TEST_BINARIES=(ruby rake rails)
 
 cd "$RAILS_TEST_APP_DIR"
 
+begin-test 'should offer commands from the Rakefile'
 (
     test-completion rake ''
     expect db:migrate db:seed test
-) || exit $?
+)
+end-test
 
+begin-test 'when the word contains a ":", should only suggest the part after that'
 (
-    test-completion rake db
-    expect db:migrate db:seed db:rollback
-) || exit $?
+    test-completion rake db:migrat
+    expect migrate:down migrate:up
 
-(
-    test-completion rake db:s
-    expect seed setup
-) || exit $?
+    test-completion rake db:migrate:
+    expect down up
+    reject db:migrate:down migrate:down
+)
+end-test
 
+begin-test 'should suggest options'
 (
     test-completion rake --
     expect --backtrace= '--help ' '--version '
-) || exit $?
+)
+end-test
--- tests/completion-ruby/test.sh
+++ tests/completion-ruby/test.sh
@@ -3,17 +3,22 @@ source "$(dirname "$0")/../test-setup"
 source "$(dirname "$0")/../../completion-ruby"
 
 _TEST_FN=__ruby
+_TEST_BINARIES=(ruby)
 
 touch foo.rb
 touch bar.rb
 touch baz.rb
 
+begin-test 'should suggest options'
 (
     test-completion ruby -
     expect -0 -e -n -x --help
-) || exit $?
+)
+end-test
 
+begin-test 'after -S should suggest commands from PATH'
 (
     test-completion ruby -S ''
     expect bundle rake
-) || exit $?
+)
+end-test
